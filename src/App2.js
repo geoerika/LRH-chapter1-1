b40f5612f8562594aeefc17f75a10ef012d930fc
Erika Szabo <erika.szabo.geo@gmail.com>
2020-04-14 19:32:59 -0400

    implemented multiple hooks

---
+++ src/App2.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+let values = []
+let currentHook = 0
+
+function useState (initialState) {
+  let hookIndex = currentHook
+  if (typeof values[currentHook] === 'undefined') values[currentHook] = initialState
+
+  function setState (nextValue) {
+    values[hookIndex] = nextValue
+    ReactDOM.render(<MyName />, document.getElementById('root'))
+  }
+
+  return [values[currentHook++], setState]
+
+}
+
+function MyName () {
+  currentHook = 0
+  const [name, setName] = useState('')
+  const [lastName, setLastName ] = useState('')
+
+  const handleChange = (evt) => {
+    setName(evt.target.value)
+  }
+  const handleLastNameChange = (evt) => {
+    setLastName(evt.target.value)
+  }
+
+  return (
+    <div>
+      <h1>My name is: { name } { lastName} </h1>
+        <input type='text' value={ name } onChange={ handleChange } />
+        <input type='text' value={lastName} onChange={handleLastNameChange} />
+    </div>
+
+  )
+}
+
+export default MyName
